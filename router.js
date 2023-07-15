3ea73091281cc4bcc1cec25f6db55461b158f9ed
Abmael PHP Engine <abmaelphpengine@gmail.com>
2023-07-15 14:08:36 -0400

    users-tsc

---
+++ router.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreateCurse = void 0;
+function CreateCurse(request, response) {
+}
+exports.CreateCurse = CreateCurse;
