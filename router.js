7ac777ecff6aa46dae5b1ce9846a0aa26c7f7d6a
Abmael PHP Engine <abmaelphpengine@gmail.com>
2023-07-15 14:22:16 -0400

    ajuste package yarn start

--- router.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateCurse = void 0;
-function CreateCurse(request, response) {
-}
-exports.CreateCurse = CreateCurse;
